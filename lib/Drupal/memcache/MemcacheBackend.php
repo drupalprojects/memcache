fdad90fdbac0c4e7dbb56676ba4c3703f8975c6d
Damian Lee <damian@damoweb.co.uk>
2013-07-09 11:30:04 -0400

    Updated .info file to yml

---
+++ lib/Drupal/memcache/MemcacheBackend.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\memcache\MemcacheBackend.
+ */
+
+namespace Drupal\memcache;
+
+use Drupal\Core\Cache\CacheBackendInterface;
+
+class MemcacheBackend implements CacheBackendInterface {
+
+}
