d3fb7fac7f05e98317e79ef1513ec51d662d67ed
Damian Lee <damian@damoweb.co.uk>
2013-07-12 18:18:22 -0400

    Initial conversion of memcache-locl-code.inc to MemcacheLockBackend service

---
+++ lib/Drupal/memcache/MemcacheLockBackend.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\memcache\MemcacheLockBackend.
+ */
+
+namespace Drupal\memcache;
+
+use Drupal\Core\Lock\LockBackendAbstract;
+
+/**
+ * Defines a Memcache lock backend.
+ */
+class MemcacheLockBackend extends LockBackendAbstract {
+
+  /**
+   * @var array
+   */
+  protected $locks = array();
+
+  /**
+   * Constructs a new MemcacheLockBackend.
+   */
+  public function __construct() {
+    // __destruct() is causing problems with garbage collections, register a
+    // shutdown function instead.
+    drupal_register_shutdown_function(array($this, 'releaseAll'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function acquire($name, $timeout = 30.0) {
+    // Ensure that the timeout is at least 1 sec. This is a limitation imposed
+    // by memcached.
+    $timeout = (int) max($timeout, 1);
+    $lock_id = $this->getLockId();
+
+    if (DrupalMemcache::add($name, $lock_id, $timeout, 'semaphore')) {
+      $this->locks[$name] = $lock_id;
+    }
+    elseif (($result = DrupalMemcache::get($name, 'semaphore')) && isset($this->locks[$name]) && ($this->locks[$name] == $lock_id)) {
+      // Only renew the lock if we already set it and it has not expired.
+      DrupalMemcache::set($name, $lock_id, $timeout, 'semaphore');
+    }
+    else {
+      // Failed to acquire the lock. Unset the key from the $locks array even if
+      // not set.
+      unset($this->locks[$name]);
+    }
+
+    return isset($this->locks[$name]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function lockMayBeAvailable($name) {
+    return !DrupalMemcache::get($name, 'semaphore');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function release($name) {
+    DrupalMemcache::delete($name, 'semaphore');
+    // We unset unconditionally since caller assumes lock is released anyway.
+    unset($this->locks[$name]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function releaseAll($lock_id = NULL) {
+    foreach ($this->locks as $name => $id) {
+      $value = DrupalMemcache::get($name, 'semaphore');
+      if ($value == $id) {
+        DrupalMemcache::delete($name, 'semaphore');
+      }
+    }
+  }
+
+}
